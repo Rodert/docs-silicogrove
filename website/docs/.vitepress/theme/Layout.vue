9a40ee1d68243ea9353a466db8a3303d45628d72
javapub <iswangshiyu@foxmail.com>
2026-08-11 23:59:38 +0800

    fix: keep a single language selector

--- website/docs/.vitepress/theme/Layout.vue
+++ website/docs/.vitepress/theme/Layout.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme'
-import LanguageSwitcher from './LanguageSwitcher.vue'
 import SiteFooter from './SiteFooter.vue'
 
 const { Layout } = DefaultTheme
@@ -8,9 +7,6 @@ const { Layout } = DefaultTheme
 
 <template>
   <Layout>
-    <template #nav-bar-content-after>
-      <LanguageSwitcher />
-    </template>
     <template #layout-bottom>
       <SiteFooter />
     </template>
